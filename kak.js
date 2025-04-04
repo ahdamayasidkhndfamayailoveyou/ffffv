b72c45ce3e75be15623bf8cd71b239f7c2962126
ahdamayasidkhndfamayailoveyou <adithyapresad123456@gmail.com>
2025-04-04 13:30:54 +0530

    Update kak.js

--- kak.js
+++ kak.js
@@ -1,4 +1,4 @@
 
 {
-  "zipmegalink": "https://mega.nz/file/Yn9UxRrI#y2J4wVNEWwbBXemjfXcCPv3Nt0QcX6aYto-38p8IOCk"
+  "zipmegalink": "https://mega.nz/file/cqtF1IbS#T169JJzGY0wKzsYjmUVx6CvvAQHDI_Az-ryTqeY_VCI"
 }
